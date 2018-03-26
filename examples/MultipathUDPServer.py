04c7e1c3359ac4cd907f081a0fffdff9106e6e8a
ytxing <ytxing@mail.ustc.edu.cn>
2018-03-26 13:13:56 +0800

    3.26写完了喂给slave
    1. 从客户端接收
    2. 多线程
    3. rsndm

--- examples/MultipathUDPServer.py
+++ examples/MultipathUDPServer.py
@@ -24,7 +24,7 @@ class Master_TCP(socket.socket):
         super().__init__()
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         assert self.s != -1
-        self.buffer = BUFFER()  # 里面是打包好的包，str类型
+        self.buffer = BUFFER()  # 里面是打包好的包，SEGMENT类型，便于后面加pathid和seq
         self.slaves = {}  # PathID:slave_config
         self.slave_start_event = {}
         self.slave_close_event = {}
@@ -52,12 +52,24 @@ class Master_TCP(socket.socket):
                                                        socket.socket(socket.AF_INET, socket.SOCK_DGRAM))
             self.slave_start_event[segment.pathid] = Event()  # 等待slave开始运行后再进行操作
 
-    def send_data_through_slave(self, pathid: int, buffer: BUFFER):
+    def send_data_through_slave(self, pathid: int, ready_to_send: BUFFER):
         if self.slaves.__contains__(pathid):
             this_slave: slave_config = self.slaves[pathid]
             self.slave_start_event[pathid].wait()  # 等待slave开始运行后再进行操作
-            while not buffer.show_first_segment()[1] == 'RELEASE_SLAVE':  # how to stop
-                this_slave.sock.sendto(buffer.get(), this_slave.slave_tuple)
+            while not ready_to_send.show_first_segment()[1] == 'RELEASE_SLAVE':  # how to stop
+                this_slave.sock.sendto(this_slave.ready_to_send.pop(0), this_slave.slave_tuple)
+
+
+    def feed_slave(self):
+        for pathid, slave_conf in self.slaves.items():
+            if self.slave_start_event[pathid].isSet():  # 该子流是否已打开
+                this_slave: slave_config = self.slaves[pathid]
+                if not self.buffer.empty():  # 是否还有没有feed的数据？
+                    raw_segment: SEGMENT = self.buffer.get()
+                    raw_segment.pathid = pathid
+                    this_slave.ready_to_send.append(raw_segment.encap())
+                else:
+                    break
 
     def slave_run(self, pathid: int):  # meixiehao!!
         t = Thread(target=self.slaves[pathid].send_data_through_slave, args=self.buffer)
@@ -65,17 +77,22 @@ class Master_TCP(socket.socket):
         self.slave_start_event[pathid].set()  # 指示该slave已经开始运行
         # threading
 
-    def feed_slave(self):
-        for pathid, slave_conf in self.slaves.items():
-            if self.slave_start_event[pathid].isSet():  # 该子流是否已打开
-                this_slave: slave_config = self.slaves[pathid]
-                if not self.buffer.empty():  # 是否还有没有feed的数据？
-                    this_slave.ready_to_send.append(self.buffer.get())
-                else:
-                    break
 
 
 m = Master_TCP()
+with open('send.txt','r') as file:
+    raw_file = file.read()
+raw_buffer = []
+seq = 0
+for i in range(0, len(raw_file), 60000):
+    m.buffer.put_raw_segment(SEGMENT(_pTYPES.CHUNK, seq, 0, 0, 1, 0, raw_file[i: i+60000])) #  没有封装成str
+    seq += 1
+    raw_buffer.append(raw_file[i: i+60000])
+
+# while i < len(raw_buffer):
+#     sub_buffer = raw_buffer[i: min(i + 60000, len(raw_buffer))]
+#     segment = SEGMENT(_pTYPES.CHUNK, seq, 0, ) # pkt_type, pkt_seq, pkt_ack, pathid, pkt_ratio, opt, pkt_data
+#     i = min(i + 60000, len(raw_buffer))
 m.wait_for_handshake()
 m.request_to_add_slave()
 print(m.slaves)
--- modules/_BUFFER.py
+++ modules/_BUFFER.py
@@ -8,13 +8,21 @@ class BUFFER(PriorityQueue):
         super().__init__()
         self.buffer = PriorityQueue(-1)
 
-    def put_segment(self, segment):
+    def put_segment_and_encap(self, segment):
         """
 
         :type segment: SEGMENT
         """
         isinstance(segment, SEGMENT)
         self.buffer.put((segment.pkt_seq, SEGMENT.encap(segment)))
+
+    def put_raw_segment(self,segment: SEGMENT):
+        """
+
+         :type segment: SEGMENT
+        """
+        isinstance(segment, SEGMENT)
+        self.buffer.put((segment.pkt_seq, segment))
 
     def show_first_segment(self) -> (int, SEGMENT):  # 返回最小编号包但不从缓存中取出
         (seq, data) = self.get()
